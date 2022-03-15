d5078651b7170a978e81fc746825b273220c9054
Wei-Yang Lin <wylin94@gmail.com>
2022-03-15 15:03:37 -0700

    header image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,33 @@ import { NavLink } from "react-router-dom";
 import './Home.css';
 
 function Home() {
-    return (
-        <div>This is Home</div>
-    )
+	return (
+		<>
+			<header className='homeHeader'>
+				<img className='homeImg' src="../images/home.ico" alt="home"></img>
+				<div>Financial Advisor</div>
+			</header>
+
+			<body>
+				<div>Please Select A Risk Level For Your Investment Portfolio</div>
+
+				<div>
+					<div>
+						<div>Low</div>
+						<div>High</div>
+					</div>
+					<div>
+						{}
+					</div>
+					<div>
+						<div>Table</div>
+						<div>button</div>
+					</div>
+				</div>
+
+			</body>
+		</>
+	)
 }
 
 export default Home;
