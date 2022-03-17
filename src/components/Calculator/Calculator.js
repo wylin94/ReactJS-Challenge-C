2f3befc422a76bfa7d69342c45719c450c34380c
Wei-Yang Lin <wylin94@gmail.com>
2022-03-16 21:33:34 -0700

    rebalance button

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,8 +6,25 @@ import './Calculator.css';
 function Calculator() {
 	const riskLabels = ['Bonds', 'Large Cap', 'Mid Cap', 'Foreign', 'Small Cap'];
 	const cpTableLabels = ['Current Amount', 'Difference', 'New Amount', 'Recommended Transfers'];
-
 	let location = useLocation();
+	const [cPInput, setCPInput] = useState([null, null, null, null, null]);
+	// const cPInputReady = cPInput.includes(null) ? false : true;
+	// console.log(cPInputReady)
+
+	const updateCPInput = (e, label) => {
+		let currentcPInput = [...cPInput];
+		if (label === 'Bonds') {currentcPInput[0] = Number(e.target.value);} 
+			else if (label === 'Large Cap') {currentcPInput[1] = Number(e.target.value);} 
+			else if (label === 'Mid Cap') {currentcPInput[2] = Number(e.target.value);} 
+			else if (label === 'Foreign') {currentcPInput[3] = Number(e.target.value);} 
+			else if (label === 'Small Cap') {currentcPInput[4] = Number(e.target.value);}
+		setCPInput(currentcPInput); 
+		console.log(cPInput)
+	}
+
+	const handleRebalance = () => {
+		console.log('rebalance clicked')
+	}
 
 	return (
 		<div className='calWrapper'>
@@ -40,7 +57,7 @@ function Calculator() {
 
 			<div className='calCPContainer'>
 				<div className='calCPLabel'>Please Enter Your Current Portfolio</div>
-				<div className='calCPButton button'>Rebalance</div>
+				<div className='calCPButton button' onClick={handleRebalance} style={cPInput.includes(null)?{opacity: 0.4}:{opacity: 1}}>Rebalance</div>
 			</div>
 
 			<div className='calCPTableContainer'>
@@ -59,9 +76,9 @@ function Calculator() {
 								<div key={label} className='calCPTableRow'>
 									<label>{label} $:</label>
 									<div className='calCPTableRowInputs'>
-										<input className='calCPTableRowCurrent'></input>
-										<input className='calCPTableRowDiff' disabled></input>
-										<input className='calCPTableRowNew' disabled></input>
+										<input className='calCPTableRowCurrent' type='text' onChange={(e) => updateCPInput(e, label)}></input>
+										<input className='calCPTableRowDiff' type='text' disabled></input>
+										<input className='calCPTableRowNew' type='text' disabled></input>
 									</div>
 								</div>
 							)
