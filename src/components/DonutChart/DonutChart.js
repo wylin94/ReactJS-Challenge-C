a82940e08c9572140fa73b8ba3c4e38eb30498cb
Wei-Yang Lin <wylin94@gmail.com>
2022-03-18 00:58:18 -0700

    update donut labels

--- src/components/DonutChart/DonutChart.js
+++ src/components/DonutChart/DonutChart.js
@@ -1,25 +1,19 @@
 import { Doughnut } from "react-chartjs-2";
 import {Chart, ArcElement} from 'chart.js'
+// import "chartjs-plugin-doughnutlabel";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 import './DonutChart.css';
 
 Chart.register(ArcElement);
+Chart.register(ChartDataLabels);
 
 function DonutChart({donutProps}) {
-	console.log('from donut chart', donutProps)
-
-	const options = {
-		// legend: {
-		// 	display: false,
-		// 	position: "right"
-		// },
-		elements: {
-			arc: {borderWidth: 0}
-		}
-	};
+	let labels = donutProps ? ['Bonds', 'Large Cap', 'Mid Cap', 'Foreign', 'Small Cap'] : ['Select Level', 'Select Level', 'Select Level', 'Select Level', 'Select Level'];
+	donutProps = donutProps ? donutProps : [1, 1, 1, 1, 1, 1];
 
 	const data = {
-		labels: ['Bonds', 'Large Cap', 'Mid Cap', 'Foreign', 'Small Cap'],
+		labels: labels,
 		datasets: [{
 			// label: 'My First Dataset',
 			data: donutProps.slice(1),
@@ -40,6 +34,41 @@ function DonutChart({donutProps}) {
 			// hoverBackgroundColor: chartColors
 			// hoverOffset: 40
 		}]
+	};
+
+	const options = {
+		legend: {
+			display: false,
+			position: "right"
+		},
+		elements: {
+			arc: {borderWidth: 0}
+		},
+    plugins: {
+      datalabels: {
+        display: true,
+        formatter: (val, ctx) => {
+          return val ? ctx.chart.data.labels[ctx.dataIndex] : '';
+        },
+				color: 'white',
+				textStrokeColor: 'black',
+				textStrokeWidth: 1.3,
+				font: {size: 18},
+
+      },
+			// doughnutlabel: {
+      //   labels: [{
+      //     text: '550',
+      //     font: {
+      //       size: 20,
+      //       weight: 'bold'
+      //     }
+      //   }, {
+      //     text: 'total'
+      //   }]
+      // }
+    }
+
 	};
 
 	return(
